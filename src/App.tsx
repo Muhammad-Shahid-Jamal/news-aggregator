0a7c1b450e63d6a12495172c77b92ccf4417c9f7
Muhammad-Shahid-Jamal <26340344+Muhammad-Shahid-Jamal@users.noreply.github.com>
2025-03-06 02:52:40 +0500

    clean the app component and restore it to basic code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-lg text-indigo-500 bg-red-800">Shahid</h1>
